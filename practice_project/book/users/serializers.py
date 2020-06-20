c03ceae53c3e5c9a9aa7fd64ce5c0f95389af7a1
徐梓琅 <ksbc24v@gmail.com>
2020-06-20 13:04:46 +0800

    更新gitignore

---
+++ practice_project/book/users/serializers.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @File  : serializers.py
+# @Author: ZRSGEEKCABIN
+# @Email : ksbc24v@gmail.com
+# @Github: https://github.com/ZRSGEEKCABIN
+# @Date  : 2020/6/19
+# @Desc  : 
+
+
+if __name__ == '__main__':
+    pass
